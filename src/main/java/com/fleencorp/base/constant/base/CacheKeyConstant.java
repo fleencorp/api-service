31ac5cd7364b6bac1034c113798f160a9b615ec7
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-07-10 15:37:51 +0100

    rename CacheKey constants for security related features

--- src/main/java/com/fleencorp/base/constant/base/CacheKeyConstant.java
+++ src/main/java/com/fleencorp/base/constant/base/CacheKeyConstant.java
@@ -7,8 +7,8 @@ public class CacheKeyConstant {
   public static final String RESET_PASSWORD_CACHE_PREFIX = "RESET_PASSWORD_TOKEN:::";
   public static final String MFA_SETUP_EMAIL_CACHE_PREFIX = "MFA_SETUP_EMAIL:::";
   public static final String MFA_SETUP_PHONE_CACHE_PREFIX = "MFA_SETUP_PHONE:::";
-  public static final String PRE_VERIFICATION_PREFIX = "PRE_VERIFICATION:::";
-  public static final String PRE_AUTHENTICATION_PREFIX = "PRE_AUTHENTICATION:::";
+  public static final String SIGN_UP_VERIFICATION_PREFIX = "SIGN_UP_VERIFICATION:::";
+  public static final String MFA_AUTHENTICATION_PREFIX = "MFA_AUTHENTICATION:::";
   public static final String UPDATE_EMAIL_CACHE_PREFIX = "UPDATE_EMAIL_ADDRESS:::";
   public static final String UPDATE_PHONE_NUMBER_CACHE_PREFIX = "UPDATE_PHONE_NUMBER:::";
   public static final String X_FORWARDED_HEADER = "X-Forwarded-For";
--- src/main/java/com/fleencorp/base/constant/base/SecurityConstant.java
+++ src/main/java/com/fleencorp/base/constant/base/SecurityConstant.java
@@ -1,11 +1,7 @@
 package com.fleencorp.base.constant.base;
 
-import static com.fleencorp.base.util.datetime.DateTimeUtil.getTimeInMillis;
-
 public final class SecurityConstant {
 
-  public static final String MFA_SECRET_LABEL = "start@fleenhistoria.com";
-  public static final String MFA_SECRET_ISSUER = "Fleen Historia";
   public static final String AUTH_HEADER_KEY = "Authorization";
   public static final String AUTH_HEADER_PREFIX = "Bearer";
 }
