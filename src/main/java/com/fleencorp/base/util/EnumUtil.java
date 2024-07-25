bb9235dfc13bc695b50cd1895ca00285d9911443
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-07-25 15:45:56 +0100

    refactor StringUtil, remove methods, add method for converting string to map

--- src/main/java/com/fleencorp/base/util/EnumUtil.java
+++ src/main/java/com/fleencorp/base/util/EnumUtil.java
@@ -17,8 +17,7 @@ public class EnumUtil {
   private static final char ENUM_VALUE_REPLACE = ' ';
 
   /**
-   * <p>Retrieves the ordinal values of an Enum class.</p>
-   * <br/>
+   * Retrieves the ordinal values of an Enum class.
    *
    * <p>This method retrieves and returns the ordinal values of the specified Enum class. It identifies the 'values' method
    * within the Enum class using reflection, retrieves all enum instances, and obtains their respective ordinal values. It
@@ -46,8 +45,7 @@ public class EnumUtil {
   }
 
   /**
-   * <p>Retrieves an enum constant of a specified type based on its ordinal value.</p>
-   * <br/>
+   * Retrieves an enum constant of a specified type based on its ordinal value.
    *
    * <p>This method fetches and returns an enum constant of a given Enum type based on its ordinal value. It retrieves all
    * enum constants of the specified type, then checks and retrieves the enum constant at the provided ordinal position.</p>
--- src/main/java/com/fleencorp/base/util/StringUtil.java
+++ src/main/java/com/fleencorp/base/util/StringUtil.java
@@ -2,65 +2,73 @@ package com.fleencorp.base.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
  * The StringUtils contains implementations and methods for converting a number represented as a string to various type of numbers
  * for example an integer or whole number.
  *
- * @author Alperen Oezkan
- * @author Shady Hussein
+ * @author Yusuf Alamu Musa
+ * @version 1.0
  */
 @Slf4j
 public class StringUtil {
 
-  private StringUtil() {
-  }
+  private StringUtil() {}
 
-  public static Long toLong(String strValue) {
-    try {
-      return Long.valueOf(strValue);
-    } catch (NumberFormatException | NullPointerException e) {
-      log.error(
-          String.format("An error occurred while trying to convert string='%s' to a Long: %s",
-              strValue,
-              e.getMessage()));
-      return null;
-    }
-  }
-
-  public static Integer toInteger(String strValue) {
-    try {
-      return Integer.valueOf(strValue);
-    } catch (NumberFormatException | NullPointerException e) {
-      log.error(
-          String.format("An error occurred while trying to convert string='%s' to a Integer: %s",
-              strValue,
-              e.getMessage()));
-      return null;
-    }
-  }
-
-  public static Float toFloat(String strValue) {
-    try {
-      return Float.valueOf(strValue);
-    } catch (NumberFormatException | NullPointerException e) {
-      log.error(
-          String.format("An error occurred while trying to convert string='%s' to a Float: %s",
-              strValue,
-              e.getMessage()));
-      return null;
-    }
-  }
-
+  /**
+   * Constructs a full name string from the given first name and last name.
+   * This method uses the overloaded getFullName method, setting the middle name to null.
+   *
+   * @param firstName the first name of the person
+   * @param lastName the last name of the person
+   * @return a concatenated full name with first and last name, with extra spaces trimmed
+   */
   public static String getFullName(String firstName, String lastName) {
     return getFullName(firstName, lastName, null);
   }
 
+  /**
+   * Constructs a full name string from the given first name, last name, and middle name.
+   *
+   * @param firstName the first name of the person
+   * @param lastName the last name of the person
+   * @param middleName the middle name of the person
+   * @return a concatenated full name, with extra spaces trimmed
+   */
   public static String getFullName(String firstName, String lastName, String middleName) {
     return
       (Objects.toString(firstName, "") + " "
       + Objects.toString(lastName, "") + " "
       + Objects.toString(middleName, "")).trim();
+  }
+
+  /**
+   * Converts a delimited string into a map of key-value pairs.
+   * The input string is split into pairs based on the specified pair separator,
+   * and each pair is further split into key and value based on the key-value separator.
+   *
+   * @param inputString the string containing the key-value pairs
+   * @param keyValueSeparator the character separating keys and values within each pair
+   * @param pairSeparator the character separating different pairs
+   * @return a map containing the key-value pairs extracted from the input string
+   */
+  public static Map<String, String> strToMap(String inputString, char keyValueSeparator, char pairSeparator) {
+    Map<String, String> map = new HashMap<>();
+    // Split the input string into key-value pairs
+    String[] pairs = inputString.split(String.valueOf(pairSeparator));
+
+    for (String pair : pairs) {
+      // Split each pair into key and value
+      String[] keyValue = pair.split(String.valueOf(keyValueSeparator));
+      if (keyValue.length == 2) {
+        // Add the key-value pair to the map after trimming whitespace
+        map.put(keyValue[0].trim(), keyValue[1].trim());
+      }
+    }
+
+    return map;
   }
 }
