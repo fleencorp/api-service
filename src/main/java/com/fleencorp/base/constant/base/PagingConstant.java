1e4caff21ca085dcc6c33044cb361fa2f9663c5a
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-09-08 10:12:16 +0100

    add base adapter

--- src/main/java/com/fleencorp/base/constant/base/PagingConstant.java
+++ src/main/java/com/fleencorp/base/constant/base/PagingConstant.java
@@ -5,4 +5,6 @@ public class PagingConstant {
   public static final Integer DEFAULT_PAGE_SIZE = 10;
   public static final String DEFAULT_SORT_BY = "id";
   public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+
 }
