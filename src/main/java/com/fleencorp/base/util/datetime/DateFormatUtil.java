5d27cc16c275dccc369b7ad4e1db87d57e76aed1
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-10-01 12:25:44 +0100

    add date time format for taking nanosecond into account

--- src/main/java/com/fleencorp/base/util/datetime/DateFormatUtil.java
+++ src/main/java/com/fleencorp/base/util/datetime/DateFormatUtil.java
@@ -18,6 +18,7 @@ public class DateFormatUtil {
   public static final String DATE = "yyyy-MM-dd";
   public static final String TIME = "[H:]mm";
   public static final String DATE_TIME_WITH_TIMEZONE = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+  public static final String DATE_TIME_WITH_TIMEZONE_NANOSECOND = "yyyy-MM-dd'T'HH:mm:ss.nnnnnnn'Z'";
   public static final String DATE_TIME_NO_SECONDS = "yyyy-MM-dd'T'HH:mm";
   public static final String DATE_TIME_NO_PT_SECONDS = "yyyy-MM-dd HH:mm";
 }
