b44d130a66a652738a31b77cae2024cadc1ca9e7
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-10-17 12:06:18 +0100

    add more target types to @IsNumber

--- src/main/java/com/fleencorp/base/validator/IsNumber.java
+++ src/main/java/com/fleencorp/base/validator/IsNumber.java
@@ -8,13 +8,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
 @Documented
 @Constraint(validatedBy = IsNumberValidator.class)
-@Target({ FIELD })
-@Retention(RUNTIME)
 public @interface IsNumber {
   String message() default "Invalid number";
 
