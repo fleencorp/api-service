51fa3d66df624dfca19b8514f2cdd603802b6ac6
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-10-17 12:07:42 +0100

    reverse changes

--- src/main/java/com/fleencorp/base/validator/IsNumber.java
+++ src/main/java/com/fleencorp/base/validator/IsNumber.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Target({ FIELD })
 @Retention(RUNTIME)
 @Documented
 @Constraint(validatedBy = IsNumberValidator.class)
